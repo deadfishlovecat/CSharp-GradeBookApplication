5f85f7d6b2e48ac34750c2eb5af815f7d87590f0
dead fish <ccc.caocongcong@outlook.com>
2020-07-28 21:35:26 +0800

    finish session3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using System;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,36 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = GradeBookType.Ranked;
+        }
+
+        /// <summary>
+        /// implement of average grade in ranked grade book
+        /// </summary>
+        /// <param name="averageGrade"></param>
+        /// <returns></returns>
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(this.Students.Count < 5)
+            {
+                throw new InvalidOperationException(
+                    "Ranked-grading requires a minimum of 5 students to work");
+            }
+            int aboveInputCount = this.Students.FindAll(s => s.AverageGrade > averageGrade).Count;
+            double percent = (double)aboveInputCount / this.Students.Count;
+            switch (percent)
+            {
+                case double per when per < 0.2:
+                    return 'A';
+                case double per when per < 0.4:
+                    return 'B';
+                case double per when per < 0.6:
+                    return 'C';
+                case double per when per < 0.8:
+                    return 'D';
+                default:
+                    return 'F';
+            }
+   
         }
     }
 }
